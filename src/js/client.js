1a21462e168cb6aee36dde99bc4e7bc1c6e5e5d3
naoko-sawada <naoko.sawada@fj.ics.keio.ac.jp>
2019-05-24 14:00:58 -0400

    merge data

--- src/js/client.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Layout from "./components/Layout";
-import './slider';
-
-const app = document.getElementById('app');
-
-ReactDOM.render(<Layout/>, app);
