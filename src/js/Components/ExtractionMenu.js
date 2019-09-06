799ff3f2ed1a29a276054dad94b04e64c074309e
naoko-sawada <naoko.sawada@fj.ics.keio.ac.jp>
2019-09-06 17:10:06 -0400

    interactively updated feature extraction menu of QBE

--- src/js/Components/ExtractionMenu.js
+++ src/js/Components/ExtractionMenu.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import VisualQuery from '../Components/VisualQuery';
 import AutomaticExtraction from '../Components/AutomaticExtraction';
+import DataStore from '../Stores/DataStore';
 
 export default class ExtractionMenu extends React.Component {
     constructor(props) {
@@ -8,8 +9,29 @@ export default class ExtractionMenu extends React.Component {
     }
 
     render() {
+        const idFile = DataStore.getAllIdsFileNames();
+        const targetList = idFile.map((data) => {
+            return (
+                <label
+                    className="form-check form-check-inline"
+                    htmlFor="inlineCheckbox1"
+                    key={data.id}>
+                    <input
+                        className="form-check-input"
+                        type="checkbox"
+                        id={"checkboxTarget" + data.id}
+                        value={data.id}
+                        key={data.id}/>
+                    {data.name}
+                    </label>
+            );
+        });
         return (
             <div>
+                <div id='targetDatasetsList' className='controllersElem'>
+                    <h6>Target datasets</h6>
+                    {targetList}
+                </div>
                 <ul className="nav nav-tabs">
                     <li className="nav-item">
                         <a className="nav-link active" data-toggle="tab" href="#automaticExtraction">Automatic Extraction</a>
--- src/js/Components/VisualQuery.js
+++ src/js/Components/VisualQuery.js
@@ -11,6 +11,8 @@ export default class Feature extends React.Component {
         this.state = {
             visualQuery: false,
             dragSelection: true,
+            queryMode: 'QBE',
+            QBESource: 'SP',
             selector: true,
             source: -1,
             selectedInterval: []
@@ -44,6 +46,30 @@ export default class Feature extends React.Component {
         FeatureAction.switchDragSelection();
     }
 
+    switchQueryMode() {
+        let mode = '';
+        if (this.state.queryMode === 'QBE') {
+            mode = 'QBS';
+        } else if (this.state.queryMode === 'QBS') {
+            mode = 'QBE';
+        }
+        this.setState({
+            queryMode: mode
+        });
+    }
+
+    switchQBESource() {
+        let source = '';
+        if (this.state.QBESource === 'SP') {
+            source = 'TT';
+        } else if (this.state.QBESource === 'TT') {
+            source = 'SP';
+        }
+        this.setState({
+            QBESource: source
+        });
+    }
+
     resetSelection() {
         FeatureAction.resetSelection();
     }
@@ -75,130 +101,184 @@ export default class Feature extends React.Component {
         // new react component
     }
 
-    render() {
-        let tubeNum = DataStore.getDataNum();
-        let featureStatus = false;
-        let visualQueryStatus = this.state.visualQuery;
-        if (tubeNum > 0)
-            featureStatus = true;
-
+    extractionSource() {
         const idFile = DataStore.getAllIdsFileNames();
         const sourceList = idFile.map((data) => {
             return <option value={data.id} key={data.id}>{data.name}</option>;
         });
-        const targetList = idFile.map((data) => {
-            return (<label className="form-check form-check-inline" htmlFor="inlineCheckbox1" key={data.id}><input className="form-check-input" type="checkbox" id={"checkboxTarget" + data.id} value={data.id} key={data.id} disabled={!visualQueryStatus}/>{data.name}</label>);
-        });
-        let selectedTimeSlice = null;
+        return (
+            <div className='featureElem'>
+                <h5>Source</h5>
+                <select
+                    className="custom-select custom-select-sm"
+                    id='sourceList'
+                    onChange={this.updateSource.bind(this)}>
+                        {sourceList}
+                </select>
+            </div>
+        );
+    }
+
+    queryModes() {
+        let QBESource;
+        if (this.state.queryMode === 'QBE') {
+            QBESource = this.QBESource();
+        }
+        return (
+            <div className='featureElem'>
+                <h5>Query mode</h5>
+                <form id='queryMode' onChange={this.switchQueryMode.bind(this)}>
+                    <div className="form-check form-check-inline">
+                        <input
+                            type="radio"
+                            name="QBE"
+                            value="QBE"
+                            checked={this.state.queryMode === 'QBE'}/>
+                        <label className="form-check-label" htmlFor="QBE">Query-by-example</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input
+                            type="radio"
+                            name="QBS"
+                            value="QBS"
+                            checked={this.state.queryMode === 'QBS'}/>
+                        <label className="form-check-label" htmlFor="QBS">Query-by-sketch</label>
+                    </div>
+                </form>
+                {QBESource}
+            </div>
+        );
+    }
+
+    QBESource() {
+        return (
+            <form id='QBESource' className='indentedRadio'onChange={this.switchQBESource.bind(this)}>
+                <div className="form-check">
+                    <input
+                        type='radio'
+                        name='QBESourceSP'
+                        value='SP'
+                        checked={this.state.QBESource === 'SP'}/>
+                    <label className="form-check-label" htmlFor='QBESourceSP'>Scatterplots</label>
+                </div>
+                <div className="form-check">
+                    <input
+                        type='radio'
+                        name='QBESourceSP'
+                        value='SP'
+                        checked={this.state.QBESource === 'TT'}/>
+                    <label className="form-check-label" htmlFor='QBESourceSP'>TimeTubes</label>
+                </div>
+            </form>
+        );
+    }
+
+    QBESelection() {
+        return (
+            <div className='featureElem'>
+                <h5>Selection</h5>
+                <form id='selector featureRow' onChange={this.switchSelector.bind(this)}>
+                    <div className="form-check form-check-inline">
+                        <input
+                            type="radio"
+                            name="pen"
+                            value="select"
+                            checked={this.state.selector}/>
+                        <label className="form-check-label" htmlFor="pen">Select</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input
+                            type="radio"
+                            name="eraser"
+                            value="Deselect"
+                            checked={!this.state.selector}/>
+                        <label className="form-check-label" htmlFor="eraser">Deselect</label>
+                    </div>
+                </form>
+                <div id='selectTimeInterval' className='form-row featureRow'>
+                    <div className="input-group input-group-sm" style={{width: '10rem', marginRight: '1.5rem'}}>
+                        <span style={{marginRight: '0.3rem'}}>Select</span>
+                        <input
+                            type="text"
+                            className="form-control custom-input"
+                            id='selectTimeIntervalInput'/>
+                        <span style={{marginLeft: '0.3rem'}}>days</span>
+                    </div>
+                    <button className="btn btn-secondary btn-sm"
+                            type="button"
+                            id='selectTimeIntervalBtn'
+                            style={{right: '0'}}
+                            onClick={this.selectTimeInterval.bind(this)} >Select</button>
+                </div>
+                <button
+                    id='resetSelectionBtn'
+                    className='btn btn-secondary btn-sm featureRow'
+                    onClick={this.resetSelection.bind(this)}>
+                    Deselect all
+                </button>
+                <h6>Options</h6>
+                <div className="form-check form-check-inline">
+                    <input
+                        className="form-check-input"
+                        type="checkbox"
+                        id="checkboxDragTube"
+                        value="option1"
+                        checked={this.state.dragSelection}
+                        onChange={this.switchDragSelection.bind(this)}/>
+                    <label
+                        className="form-check-label"
+                        htmlFor="inlineCheckbox1">
+                        Selection by drag
+                    </label>
+                </div>
+            </div>
+        );
+    }
+
+    selectionDetail() {
+        let selectedTimeSlice;
         if (this.state.source >= 0) {
             selectedTimeSlice = <SelectedTimeSlice sourceId={this.state.source} selectedInterval={this.state.selectedInterval}/>;
         }
         return (
+            <div className='featureElem'>
+                <h5>Selection Detail</h5>
+                <span id='selectedInterval'></span>
+                <div id='selectedIntervalViewArea'>
+                    {selectedTimeSlice}
+                    {/*<SelectedTimeSlice sourceId={this.state.source} selectedInterval={this.state.selectedInterval}/>*/}
+                </div>
+            </div>
+        );
+    }
+
+    render() {
+        let queryDefinition;
+        if (this.state.queryMode === 'QBE') {
+            queryDefinition = this.QBESelection();
+        } else if (this.state.queryMode === 'QBS') {
+            
+        }
+        return (
             <div id='featureArea' className='controllersElem'>
-                <h5>FEATURE EXTRACTION</h5>
-                <div className="custom-control custom-switch featureElem">
-                    <input
-                        type="checkbox"
-                        className="custom-control-input"
-                        id="switchVisualQuery"
-                        checked={visualQueryStatus}
-                        disabled={!featureStatus}
-                        onClick={this.switchVisualQuery.bind(this)}/>
-                        <label
-                            className="custom-control-label"
-                            htmlFor="switchVisualQuery">
-                            Visual query
-                        </label>
-                </div>
-                <div className='featureElem'>
-                    <h6>Source</h6>
-                    <select
-                        className="custom-select custom-select-sm"
-                        id='sourceList'
-                        disabled={!this.state.visualQuery}
-                        onChange={this.updateSource.bind(this)}>
-                        {sourceList}
-                    </select>
-                </div>
-                <div className='featureElem'>
-                    <h6>Target</h6>
-                    <div id='targetList' onChange={this.updateTarget.bind(this)}>
-                        {targetList}
-                    </div>
-                </div>
-                <div className='featureElem'>
-                    <h6>Selection</h6>
-                    <form id='selector featureRow' onChange={this.switchSelector.bind(this)}>
-                        <div className="form-check form-check-inline">
-                            <input
-                                type="radio"
-                                name="pen"
-                                value="select"
-                                disabled={!visualQueryStatus}
-                                checked={this.state.selector}/>
-                            <label className="form-check-label" htmlFor="inlineCheckbox1">Select</label>
-                        </div>
-                        <div className="form-check form-check-inline">
-                            <input
-                                type="radio"
-                                name="eraser"
-                                value="Deselect"
-                                disabled={!visualQueryStatus}
-                                checked={!this.state.selector}/>
-                            <label className="form-check-label" htmlFor="inlineCheckbox1">Deselect</label>
-                        </div>
-                    </form>
-                    <div id='selectTimeInterval' className='form-row featureRow'>
-                        <div className="input-group input-group-sm" style={{width: '10rem', marginRight: '1.5rem'}}>
-                            <span style={{marginRight: '0.3rem'}}>Select</span>
-                            <input
-                                type="text"
-                                className="form-control custom-input"
-                                id='selectTimeIntervalInput'
-                                disabled={!visualQueryStatus}/>
-                            <span style={{marginLeft: '0.3rem'}}>days</span>
-                        </div>
-                        <button className="btn btn-secondary btn-sm"
-                                type="button"
-                                id='selectTimeIntervalBtn'
-                                style={{right: '0'}}
-                                disabled={!visualQueryStatus}
-                                onClick={this.selectTimeInterval.bind(this)} >Select</button>
-                    </div>
-                    <button
-                        id='resetSelectionBtn'
-                        className='btn btn-secondary btn-sm featureRow'
-                        disabled={!visualQueryStatus}
-                        onClick={this.resetSelection.bind(this)}>
-                        Deselect all
-                    </button>
-                </div>
-                <div className='featureElem'>
-                    <h6>Options</h6>
-                    <div className="form-check form-check-inline">
-                        <input
-                            className="form-check-input"
-                            type="checkbox"
-                            id="checkboxDragTube"
-                            value="option1"
-                            checked={this.state.dragSelection}
-                            disabled={!visualQueryStatus}
-                            onChange={this.switchDragSelection.bind(this)}/>
-                        <label
-                            className="form-check-label"
-                            htmlFor="inlineCheckbox1">
-                            Selection by drag
-                        </label>
-                    </div>
-                </div>
-                <div className='featureElem'>
-                    <h6>Selection Detail</h6>
-                    <span id='selectedInterval'></span>
-                    <div id='selectedIntervalViewArea'>
-                        {selectedTimeSlice}
-                        {/*<SelectedTimeSlice sourceId={this.state.source} selectedInterval={this.state.selectedInterval}/>*/}
-                    </div>
-                </div>
+                {/*<div className="custom-control custom-switch featureElem">*/}
+                    {/*<input*/}
+                        {/*type="checkbox"*/}
+                        {/*className="custom-control-input"*/}
+                        {/*id="switchVisualQuery"*/}
+                        {/*checked={visualQueryStatus}*/}
+                        {/*disabled={!featureStatus}*/}
+                        {/*onClick={this.switchVisualQuery.bind(this)}/>*/}
+                        {/*<label*/}
+                            {/*className="custom-control-label"*/}
+                            {/*htmlFor="switchVisualQuery">*/}
+                            {/*Visual query*/}
+                        {/*</label>*/}
+                {/*</div>*/}
+                {this.extractionSource()}
+                {this.queryModes()}
+                {queryDefinition}
+                {this.selectionDetail()}
             </div>
         );
     }
