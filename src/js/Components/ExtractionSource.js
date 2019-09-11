60352c1f09460814c8ed00526a1082cf819d19fa
naoko-sawada <naoko.sawada@fj.ics.keio.ac.jp>
2019-09-10 22:26:20 -0400

    create a SP for lightcurve on the extraction source panel

--- src/js/Components/ExtractionSource.js
+++ src/js/Components/ExtractionSource.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import * as d3 from 'd3';
+import DataStore from '../Stores/DataStore';
 import FeatureStore from '../Stores/FeatureStore';
+import Scatterplots from './Scatterplots';
 
 export default class ExtractionSource extends React.Component {
     constructor(props) {
         super();
         this.state = {
+            data: DataStore.getAllData(),
             source: -1
         };
     }
 
     componentWillMount() {
+        DataStore.on('upload', () => {
+            this.setState({
+                data: DataStore.getAllData()
+            });
+        });
         FeatureStore.on('updateSource', () => {
             let sourceId = FeatureStore.getSource();
             this.setState({
@@ -43,16 +51,43 @@ export default class ExtractionSource extends React.Component {
             .append('div')
             .attr('id', divId)
             .html(content);
-        // resize SP
     }
 
     render() {
+        // draw scatterplots for lightcurve whenever a new file is uploaded
+        // let size = $('#QBESource').outerWidth(true) - Number($('#QBESource').css('padding-left').replace('px', '')) * 2;
+        let size = 500;
+        if (document.getElementById('QBESource') !== null) {
+            let tmp = $('#QBESource').outerWidth(true) - Number($('#QBESource').css('padding-left').replace('px', '')) * 2;
+            if (tmp > 0)
+                size = tmp;
+        }
+        let scatterplots = this.state.data.map((data) => {
+            return (
+                <div
+                    key={data.id}
+                    id={'QBE_SP_' + data.id}
+                    style={{
+                        display: 'inline-block',
+                        visibility: (data.id === Number(this.state.source)) ? 'visible': 'hidden'}}>
+                    <Scatterplots
+                        key={data.id}
+                        id={data.id}
+                        divID={'QBE_SP_' + data.id}
+                        xItem={'z'}
+                        yItem={'V'}
+                        width={size}
+                        height={Math.ceil(size * 0.8)}/>
+                </div>
+            );
+        });
         return (
             <div
                 id='QBESourceArea'>
                 <div id='QBESourceTT'>
                 </div>
                 <div id='QBESourceSP'>
+                    {scatterplots}
                 </div>
             </div>
         );
