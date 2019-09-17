b0ded4766e666c92a4d6cfe53ad9fdbeb2628a6b
naoko-sawada <naoko.sawada@fj.ics.keio.ac.jp>
2019-09-16 22:41:23 -0400

    change to update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,55 @@
-var debug = process.env.NODE_ENV !== "production";
-var webpack = require('webpack');
-var path = require('path');
-
+// var debug = process.env.NODE_ENV !== "production";
+// var webpack = require('webpack');
+// var path = require('path');
+//
+// module.exports = {
+//     context: path.join(__dirname, "src"),
+//     devtool: debug ? "inline-sourcemap" : null,
+//     entry: "./js/client.js",
+//     module: {
+//         loaders: [
+//             {
+//                 test: /\.jsx?$/,
+//                 exclude: /(node_modules|bower_components)/,
+//                 loader: 'babel-loader',
+//                 query: {
+//                     presets: ['react', 'es2015', 'stage-0'],
+//                     plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
+//                 }
+//             }
+//         ]
+//     },
+//     output: {
+//         path: __dirname + "/src/",
+//         filename: "client.min.js"
+//     },
+//     plugins: debug ? [] : [
+//         new webpack.optimize.DedupePlugin(),
+//         new webpack.optimize.OccurenceOrderPlugin(),
+//         new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+//     ],
+// };
 module.exports = {
-    context: path.join(__dirname, "src"),
-    devtool: debug ? "inline-sourcemap" : null,
-    entry: "./js/client.js",
+    entry: "./src/js/client.js",
+    output: {
+        path:__dirname+ '/src/',
+        filename: "client.min.js",
+        publicPath: '/'
+    },
+    devServer: {
+        contentBase: "./src",
+    },
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
+                exclude:/(node_modules|bower_components)/,
                 loader: 'babel-loader',
                 query: {
-                    presets: ['react', 'es2015', 'stage-0'],
-                    plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
+                    presets: ['es2015', 'react']
                 }
             }
         ]
-    },
-    output: {
-        path: __dirname + "/src/",
-        filename: "client.min.js"
-    },
-    plugins: debug ? [] : [
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-    ],
+    }
+
 };
