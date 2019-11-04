a106cd541f1e9b93a83513d7211e687f559094bb
naoko-sawada <naoko.sawada@fj.ics.keio.ac.jp>
2019-11-04 00:12:28 -0500

    separate formatValue functions from each component to library

--- src/js/lib/2DGraphLib.js
+++ src/js/lib/2DGraphLib.js
@@ -1,7 +1,18 @@
-export function tickFormatting(d) {
-    let result = d;
-    if (Math.log10(Math.abs(d)) < -2 && d !== 0) {
-        result = d.toExponential(0);
+export function tickFormatting(value) {
+    let result = value;
+    if (Math.log10(Math.abs(value)) < -2 && value !== 0) {
+        result = value.toExponential(0);
+    }
+    return result;
+}
+
+export function formatValue(value) {
+    value = Number(value);
+    let result;
+    if (Math.log10(Math.abs(value)) < -2 && value !== 0) {
+        result = value.toExponential(1);
+    } else {
+        result = value.toFixed(2);
     }
     return result;
 }
