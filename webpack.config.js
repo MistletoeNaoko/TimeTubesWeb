2ed3f4b4343af453deb7bb6ee354f5be3361eba2
naoko-sawada <naoko.sawada@fj.ics.keio.ac.jp>
2020-05-25 21:41:46 +0900

    update babel

--- webpack.config.js
+++ webpack.config.js
@@ -46,9 +46,12 @@ module.exports = {
                 test: /\.jsx?$/,
                 exclude:/(node_modules|bower_components)/,
                 loader: 'babel-loader',
-                query: {
-                    presets: ['es2015', 'react']
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
                 }
+                // query: {
+                //     presets: ['es2015', 'react']
+                // }
             }
         ]
     },
