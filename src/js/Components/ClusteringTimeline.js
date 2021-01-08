4017253dc94e9178242fffda4a4711e3d7779d02
naoko-sawada <naoko.sawada@fj.ics.keio.ac.jp>
2021-01-08 18:20:28 +0900

    divide clustering result timelines by cluster

--- src/js/Components/ClusteringTimeline.js
+++ src/js/Components/ClusteringTimeline.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as d3 from 'd3';
+import {resizeExtractionResultsArea} from '../Actions/AppAction';
 import DataStore from '../Stores/DataStore';
 import ClusteringStore from '../Stores/ClusteringStore';
 import AppStore from '../Stores/AppStore';
@@ -83,15 +84,20 @@ export default class ClusteringTimeline extends React.Component {
             let timeline = timelineArea.append('svg')
                 .attr('id', 'clusteringTimelineSVG_' + this.datasets[i])
                 .attr('class', 'clusteringTimelineSVG')
-                .attr('width', clientWidth)
+                .attr('width', width)
                 .attr('height', 40);
             let fileName = timeline
                 .append('text')
                 .attr('x', 0)
                 .attr('y', height / 2)
+                .attr('id', 'clusteringTimeline_' + this.datasets[i])
                 .attr('text-anchor', 'left')
                 .style('font-size', '10px')
-                .text(data.name);
+                .text(data.name)
+                .on('click', expandTimeline().bind(this));
+                // .on('click', function(data,idx,elem) {
+                //     console.log(data, idx, elem, elem[0].id)
+                // });//this.expandTimeline());
             let xScale = d3.scaleLinear()
                 .domain([data.data.meta.min.z, data.data.meta.max.z])
                 .nice()
@@ -100,7 +106,7 @@ export default class ClusteringTimeline extends React.Component {
                 .ticks(10)
                 .tickSize(10);
             let xAxis = timeline.append('g')
-                .attr('class', 'x-axis')
+                .attr('class', 'x-axis')// clusteringTimelineXAxis_' + this.datasets[i])
                 .attr('id', 'clusteringTimelineXAxis_' + this.datasets[i])
                 .attr('transform', 'translate(' + (this.margin.left + fileNameWidth) + ',' + (height / 2 - 5) + ')')
                 .call(xLabel);
@@ -119,7 +125,7 @@ export default class ClusteringTimeline extends React.Component {
                     }.bind(this)))
                     .enter()
                     .append('line')
-                    .attr('class', 'clusterLine_' + this.datasets[j] + '_' + i)
+                    .attr('class', 'clusterLine_' + this.datasets[j] + ' clusterLine_' + this.datasets[j] + '_' + i)
                     .attr('x1', function(d) {
                         return this.xScales[j](this.subsequences[d][0].z);
                     }.bind(this))
@@ -133,6 +139,56 @@ export default class ClusteringTimeline extends React.Component {
                     .attr('opacity', 0.5)
                     .attr('transform', 'translate(' + (this.margin.left + fileNameWidth) + ',0)');
             }
+        }
+        function expandTimeline() {
+            return function(data, idx, elem) {
+                // show a timeline for each cluster
+                let dataId = Number(elem[0].id.split('_')[1]);
+                if (Number(this.timelines[dataId].attr('height')) === height) {
+                    // expand
+                    this.timelines[dataId]
+                        .transition()
+                        .duration(1000)
+                        .attr('height', height * this.clusterCenters.length);
+                    for (let i = 0; i < this.clusterCenters.length; i++) {
+                        if (i !== 0) {
+                            let axis = this.timelines[dataId].append('g')
+                                .attr('class', 'x-axis clusteringTimelineXAxis_' + dataId)
+                                .attr('id', 'clusteringTimelineXAxis_' + dataId + '_' + i)
+                                .attr('transform', 'translate(' + (this.margin.left + fileNameWidth) + ',' + (height / 2 - 5) + ')')
+                                .transition()
+                                .duration(1000)
+                                .attr('transform', 'translate(' + (this.margin.left + fileNameWidth) + ',' + (height * i + height / 2 - 5) + ')')
+                                .call(this.xLabels[dataId]);
+                        }
+                        this.timelines[dataId]
+                            .selectAll('line.clusterLine_' + dataId + '_' + i)
+                            .transition()
+                            .duration(1000)
+                            .attr('transform', 'translate('　+ (this.margin.left + fileNameWidth) + ',' + (height * i) + ')');
+                    }
+                } else {
+                    // collapse
+                    this.timelines[dataId]
+                        .selectAll('line.clusterLine_' + dataId)
+                        .transition()
+                        .duration(1000)
+                        .attr('transform', 'translate('　+ (this.margin.left + fileNameWidth) + ',0)');
+                    d3.selectAll('.clusteringTimelineXAxis_' + dataId)
+                        .transition()
+                        .duration(1000)
+                        .attr('translate(' +  + (this.margin.left + fileNameWidth) + ',' + (height / 2 - 5) + ')');
+                    d3.selectAll('.clusteringTimelineXAxis_' + dataId)
+                        .transition()
+                        .delay(1000)
+                        .remove();
+                    this.timelines[dataId]
+                        .transition()
+                        .duration(1000)
+                        .attr('height', height);
+                }
+                resizeExtractionResultsArea();
+            };
         }
     }
 
@@ -153,6 +209,8 @@ export default class ClusteringTimeline extends React.Component {
                 .tickSize(10);
             this.xAxes[i]
                 .call(this.xLabels[i]);
+            d3.selectAll('.x-axis.clusteringTimelineXAxis_' + this.datasets[i])
+                .call(this.xLabels[i]);
             for (let j = 0; j < this.clusters.length; j++) {
                 d3.selectAll('line.clusterLine_' + this.datasets[i] + '_' + j)
                     .attr('x1', function(d) {
@@ -160,7 +218,7 @@ export default class ClusteringTimeline extends React.Component {
                     }.bind(this))
                     .attr('x2', function(d) {
                         return this.xScales[i](this.subsequences[d][this.subsequences[d].length - 1].z);
-                    }.bind(this))
+                    }.bind(this));
             }
         }
     }
