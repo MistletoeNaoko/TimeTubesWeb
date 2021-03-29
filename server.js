4e1ad5a3107781d0a89b54688885ace5f0fa2bcd
澤田奈生子 <naokosawada@naokosawada.local>
2021-03-30 00:22:20 +0900

    add host to client.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var path = require('path');
 var express = require('express');
 const PORT = process.env.PORT || 5000
+const host = '0.0.0.0';
 
 var app = express();
 
 app.use(express.static(path.join(__dirname, '/src')))
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+    .listen(PORT, host, () => console.log(`Listening on ${ PORT }`));
 
 // var server = app.listen(app.get('port'), function() {
 //     console.log('listening on port ', server.address().port);
