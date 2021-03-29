71ca543922ab67320cb321dc34f7a6c86b05a88e
澤田奈生子 <naokosawada@naokosawada.local>
2021-03-29 23:40:03 +0900

    change port num

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(path.join(__dirname, '/src')));
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 5000);
 
 var server = app.listen(app.get('port'), function() {
     console.log('listening on port ', server.address().port);
